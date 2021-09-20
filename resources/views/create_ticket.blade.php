8f17d5ec3ff1f3dfd7fa955aaf451e7835538490
Ryosuke-Shigi <adalovelace6365@gmail.com>
2021-09-20 14:27:36 +0900

    transaction tables commit

--- resources/views/create_ticket.blade.php
+++ resources/views/create_ticket.blade.php
@@ -33,64 +33,65 @@
         <div class="itemname_line">重要注意事項</div>
         <div class="itemsector"><input class="textbox" type="text" id="important_notes" name="important_notes"></div>
         <div class="itemname_line">チケット紹介</div>
-        <div class="itemsector"><input class="textbox" type="text" id="ticket_inquiry" name="ticket_inquiry_image"></div>
+        <div class="itemsector"><input class="textbox" type="text" id="contents_type" name="contents_type"></div>
         <div class="itemname_line">紹介画像</div>
-        <div class="itemsector"><input class="textbox" type="text" id="inquiry_image" name="inquiry_image"></div><div class="buttonsector"></div>
+        <div class="itemsector"><input class="textbox" type="text" id="contents_image" name="contents_image"></div><div class="buttonsector"></div>
         <div class="itemname_line">注意事項</div>
-        <div class="itemsector"><input class="textbox" type="text" id="notes" name="notes"></div>
+        <div class="itemsector"><input class="textbox" type="text" id="detail_notes" name="detail_notes"></div>
         <div class="itemname_line">お問合せ</div>
         <div class="itemsector"><input class="textbox" type="text" id="ticket_remarks" name="ticket_remarks"></div>
 
     <div class="contenttitle">商品設定</div>
         <div class="itemname_line">商品番号</div>
-        <div class="itemsector"><input class="textbox" type="text" id="item_number" name="item_number"></div><div class="buttonsector"></div>
+        <div class="itemsector"><input class="textbox" type="text" id="ticket_code" name="ticket_code"></div><div class="buttonsector"></div>
         <div class="itemname_line">確認オプション</div>
         <div class="itemsector"><input type="checkbox" id="minors_flag" name="minors_flag"><label for="minors_flag">未成年フラグ</label></div><div class="buttonsector"></div>
         <div class="itemname_line">キャンセル料<br>発生期限（分）</div>
-        <div class="itemsector"><input class="textbox" type="text" id="cancel_limite" name="cancel_limite"></div>
+        <div class="itemsector"><input class="textbox" type="text" id="cancel_limit" name="cancel_limit"></div>
         <div class="itemname_line">料金</div>
         <div class="price_section">
             <div class="item_section">
                 <div class="item_name">名称１</div>
-                <input class="item_text" type="text" id="name01" name="name01">
+                <input class="item_text" type="text" id="type_name01" name="type_name01">
             </div>
             <div class="item_section">
                 <div class="item_name">単価１</div>
-                <input class="item_text" type="text" id="unit_price01" name="unit_price01">
+                <input class="item_text" type="text" id="type_money01" name="type_money01">
             </div>
             <div class="item_section">
                 <div class="item_name">キャンセル料１</div>
-                <input class="item_text" type="text" id="cancel_price01" name="cancel_price01">
+                <input class="item_text" type="text" id="cancel_rate01" name="cancel_rate01">
             </div>
             <div class="item_section">
                 <div class="item_name">名称２</div>
-                <input class="item_text" type="text" id="name02" name="name01">
+                <input class="item_text" type="text" id="type_name02" name="type_name02">
             </div>
             <div class="item_section">
                 <div class="item_name">単価２</div>
-                <input class="item_text" type="text" id="unit_price02" name="unit_price02">
+                <input class="item_text" type="text" id="type_money02" name="type_money02">
             </div>
             <div class="item_section">
                 <div class="item_name">キャンセル料２</div>
-                <input class="item_text" type="text" id="cancel_price02" name="cancel_price02">
+                <input class="item_text" type="text" id="cancel_rate02" name="cancel_rate02">
             </div>
         </div>
-
+        <div class="itemname_line">注意事項</div>
+        <div class="itemsector"><input class="textbox" type="text" id="item_notes" name="item_notes"></div>
 
     <div class="contenttitle">チケット設定</div>
-        <input type="radio" name="ticket_type" id="ticket_free" name="ticket_free"><label for="ticket_free">フリーチケット</label>
-        <input type="radio" name="ticket_type" id="ticket_specified" name="ticket_specified"><label for="ticket_specified">指定チケット</label>
+        <input type="radio" name="tickets_kind" id="ticket_free" value="1"><label for="ticket_free">フリーチケット</label>
+        <input type="radio" name="tickest_kind" id="ticket_specified" value="2"><label for="ticket_specified">指定チケット</label>
 
     <div class="contenttitle">サービス設定</div>
     <div class="itemname_line">サービス名</div>
-    <div class="itemsector"><input class="textbox" type="text" id="service_name" name="service_name"></div>
+    <div class="itemsector"><input class="textbox" type="text" id="svc_name" name="svc_name"></div>
     <div class="itemname_line">内容・注意事項</div>
-    <div class="itemsector"><input class="textbox" type="text" id="service_content01" name="service_content01"></div>
+    <div class="itemsector"><input class="textbox" type="text" id="svc_cautions01" name="service_content01"></div>
     <div class="itemname_line"></div>
-    <div class="itemsector"><input class="textbox" type="text" id="service_content02" name="service_content02"></div><div class="buttonsector"></div>
+    <div class="itemsector"><input class="textbox" type="text" id="svc_cautions02" name="service_content02"></div><div class="buttonsector"></div>
     <div class="itemname_multi">サービス種類</div>
-        <input type="radio" name="service_type" id="service01" name="service01"><label for="service01">サービス１</label>
-        <input type="radio" name="service_type" id="service02" name="service02"><label for="service02">サービス２</label>
+        <input type="radio" name="svc_type" id="svc_type01" value="1"><label for="svc_type01">サービス１</label>
+        <input type="radio" name="svc_type" id="svc_type02" value="2"><label for="svc_type02">サービス２</label>
 
     <!--登録・戻るボタン-->
     <div class="footer">
