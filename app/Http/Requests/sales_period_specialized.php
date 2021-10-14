a4446d7d940683e3826a6633d9a4810e874d0241
Ryosuke-Shigi <adalovelace6365@gmail.com>
2021-10-14 16:00:31 +0900

    specialized

--- app/Http/Requests/sales_period_specialized.php
+++ app/Http/Requests/sales_period_specialized.php
@@ -13,7 +13,7 @@ class sales_period_specialized extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -25,6 +25,29 @@ class sales_period_specialized extends FormRequest
     {
         return [
             //
+            //共通
+            'sales_interval_start_date'=>'required',
+            'sales_interval_start_times'=>'required',
+            'sales_interval_end_date'=>'required',
+            'sales_interval_end_times'=>'required',
+
+            //指定チケット
+            'ticket_interval'=>'required|integer',
+            'ticket_buy_date'=>'required',
+
+            //共通
+            'ticket_num'=>'required|integer',
+            'ticket_min_num'=>'required|integer',
+            'ticket_max_num'=>'required|integer'
+        ];
+    }
+
+
+    public function message(){
+        return [
+            'ticket_interval.integer'=>'数字を入力してください',
+            'ticket_min_num.integer'=>'数字を入力してください',
+            'ticket_max_num.integer'=>'数字を入力してください'
         ];
     }
 }
